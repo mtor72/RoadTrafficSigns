a25c07a03941cd1341b670b8fa47ee6ea2d0bc98
Михаил Торбин <mltor8@gmail.com>
2023-07-16 19:13:58 -0400

    files added

---
+++ src/test/java/HomePageTests.java
@@ -0,0 +1,43 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import pages.HomePage;
+
+public class HomePageTests extends TestBase {
+    /* Navigate to Home Page
+     *  Click on Custom Traffic Signs Image Box
+     *  Check Custom Traffic Signs page appears
+     *  Check Custom Traffic Signs page Heading as expected */
+    @Test
+    public void homePage_ClickOnCustomTrafficSignsImageBox_CustomTrafficSignsPageAppears() {
+        // arrange
+        var homePage = new HomePage(driver, wait);
+
+        // act
+        homePage.customTrafficSignsImageBox.click();
+
+        // assert
+        Assertions.assertTrue(homePage.isPageTitleCustomTrafficSigns(), "Custom Traffic Signs page doesn't appear");
+        var expectedPageHeading = "Custom Traffic Signs";
+        Assertions.assertEquals(expectedPageHeading, homePage.getCustomTrafficSignsPageHeadingText(),
+                "Custom Traffic Signs heading doesn't appear");
+    }
+
+    /* Navigate to Home Page
+     *  Click on Custom Traffic Signs Text Link
+     *  Check Custom Traffic Signs page appears
+     *  Check Custom Traffic Signs page Heading as expected */
+    @Test
+    public void homePage_ClickOnCustomTrafficSignsTextLink_CustomTrafficSignsPageAppears() {
+        // arrange
+        var homePage = new HomePage(driver, wait);
+
+        // act
+        homePage.customTrafficSignsTextLink.click();
+
+        // assert
+        Assertions.assertTrue(homePage.isPageTitleCustomTrafficSigns(), "Custom Traffic Signs page doesn't appear");
+        var expectedPageHeading = "Custom Traffic Signs";
+        Assertions.assertEquals(expectedPageHeading, homePage.getCustomTrafficSignsPageHeadingText(),
+                "Custom Traffic Signs heading doesn't appear");
+    }
+}
